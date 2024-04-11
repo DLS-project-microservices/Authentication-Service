6261e51f81dd9e68184149b5fdcd7f4c37bded30
masp1234 <mart2c04@stud.kea.dk>
2024-04-11 16:40:55 +0200

    removed routing key, since it is not needed

--- messages/userMessage.js
+++ messages/userMessage.js
@@ -4,15 +4,15 @@ async function publishUserEvent(user, userStatus) {
     if (!user || !userStatus) {
         throw new Error('Invalid parameters: need to have both user and userstatus arguments');
     }
-    console.log(user)
 
     const { channel, exchange } = await connect();
     const message = { 
         status: userStatus,
         user: user
     }
+    
     console.log(message);
-    channel.publish(exchange, 'user change', Buffer.from(JSON.stringify(message)));
+    channel.publish(exchange, '', Buffer.from(JSON.stringify(message)));
 }
 
 export {
