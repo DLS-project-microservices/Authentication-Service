4ed842f632a776bd02f40af9b84f0c8a0c7cbd28
masp1234 <mart2c04@stud.kea.dk>
2024-05-15 00:58:35 +0200

    using retry wrapper

--- messages/userMessage.js
+++ messages/userMessage.js
@@ -1,11 +1,17 @@
-import connect from './connection.js';
+import { connectToRabbitMQ } from 'amqplib-retry-wrapper-dls';
+
+const channel = await connectToRabbitMQ(process.env.AMQP_HOST);
+const exchange = 'user'
 
 async function publishUserEvent(user, userStatus) {
     if (!user || !userStatus) {
         throw new Error('Invalid parameters: need to have both user and userstatus arguments');
     }
 
-    const { channel, exchange } = await connect();
+    await channel.assertExchange(exchange, 'fanout', {
+        durable: true
+    });
+
     const message = { 
         status: userStatus,
         user: user
